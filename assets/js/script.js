774d69b9ff2999e4e349484a80c6336e3c01e32e
farisqlail <faris.riskilail@gmail.com>
2023-12-30 14:44:07 +0700

    first commit for template

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.classList.add("navbar-fixed");
+    } else {
+        navbar.classList.remove("navbar-fixed");
+    }
+}
